170f9b6ab7da631dea8db834ca5a3c10d25d4e8e
nw11 <nicolas.walker@gmail.com>
2017-05-05 19:18:32 +0100

    first commit

---
+++ src/setup.sh
@@ -0,0 +1,14 @@
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+bash Miniconda2-latest-Linux-x86_64.sh -b
+export PATH=/home/dnanexus/miniconda2/bin:$PATH
+ 
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
+conda install deeptools
+
+wget https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.1-linux-x86_64.tar.gz
+tar -xvf julia-0.5.1-linux-x86_64.tar.gz
+export PATH=/home/dnanexus/julia-6445c82d00/bin:$PATH
+
