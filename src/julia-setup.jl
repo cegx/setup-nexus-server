1f0d26f67d5f38df7b3272b47b9e08ff2509a4ab
nw11 <nicolas.walker@gmail.com>
2017-05-05 20:15:44 +0100

    add julia packages to load

---
+++ src/julia-setup.jl
@@ -0,0 +1,18 @@
+
+Pkg.add("Plots")
+Pkg.add("DataTables")
+Pkg.add("DataFrames")
+Pkg.add("Memento")
+Pkg.add("StatPlots")
+Pkg.add("RCall")
+Pkg.add("MultipleTesting")
+
+using Plots
+using DataTables
+using DataFrames
+using Memento
+using StatPlots
+using RCall
+using MultipleTesting
+
+
