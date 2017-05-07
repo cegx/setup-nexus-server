9fa90abed08b84585b3a487de92b381358cc55d3
nw11 <nicolas.walker@gmail.com>
2017-05-07 12:40:56 +0100

    add to bin dirs to bashrc

--- src/setup.sh
+++ src/setup.sh
@@ -9,16 +9,18 @@ conda config --add channels bioconda
 conda install -y deeptools
 conda install -y R
 conda install -y wiggletools
+conda install -y epic
 
 # move the old default version of R, so we use conda R instead
 sudo mv /usr/bin/R /usr/bin/R2
 
 export PATH=/home/dnanexus/miniconda2/bin:$PATH
- 
+echo "export PATH=/home/dnanexus/miniconda2/bin:$PATH" >> ~/.bashrc
 
 wget https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.1-linux-x86_64.tar.gz
 tar -xvf julia-0.5.1-linux-x86_64.tar.gz
 export PATH=/home/dnanexus/julia-6445c82d00/bin:$PATH
-
+# so other byobu screens have julia
+echo "export PATH=/home/dnanexus/julia-6445c82d00/bin:$PATH" >> ~/.bashrc
 julia /home/dnanexus/setup-nexus-server/src/julia-setup.jl
 
