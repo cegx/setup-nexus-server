cd8b5b8330ae4e6487e26a4feec8b0539a675e44
nw11 <nicolas.walker@gmail.com>
2017-05-12 18:24:18 +0100

    install GNU parallel

--- src/setup.sh
+++ src/setup.sh
@@ -1,7 +1,7 @@
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh -b
 export PATH=/home/dnanexus/miniconda2/bin:$PATH
- 
+
 conda config --add channels conda-forge
 conda config --add channels defaults
 conda config --add channels r
@@ -28,3 +28,4 @@ export PATH=/home/dnanexus/julia-6445c82d00/bin:$PATH
 echo "export PATH=/home/dnanexus/julia-6445c82d00/bin:$PATH" >> ~/.bashrc
 julia /home/dnanexus/setup-nexus-server/src/julia-setup.jl
 
+sudo apt-get install parallel
